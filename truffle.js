16b9d6cb2d5aca39cddcadd4be50cd4d3562fc09
Adam Dossa <adam.dossa@gmail.com>
2017-10-20 13:15:11 +0100

    Add TokenABI.txt

--- truffle.js
+++ truffle.js
@@ -4,6 +4,7 @@ module.exports = {
       host: "localhost",
       port: 8545,
       gasPrice: 5000000000,
+      from: "0xf8536ca7a25cbf70df754fa310079ada4c6114c2",
       network_id: "*" // Match any network id
     }
   }
